68c3ad2e791b0f5d03ae2bf61e35905021f562ad
MOSCOW\Daniil_Bogdanov <daniil_bogdanov@epam.com>
2022-08-15 19:12:28 +0400

    test

--- HW8/01_mock_test.py
+++ HW8/01_mock_test.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest import mock
+from unittest.mock import patch
 from Goldbachs import (even, eratosthenes, goldbach, 
                        odd_primes, CustomError)
 
@@ -8,19 +8,19 @@ class gbtest(unittest.TestCase):
         ev=even(8)
         self.assertEqual(ev, (True, 8))
         
-    @mock.patch('builtins.input')    
+    @patch('builtins.input')    
     def test_even_word(self, mock_input):
         mock_input.return_value="asd"
         with self.assertRaises(ValueError):
             even('asd')
         
-    @mock.patch('builtins.input')    
+    @patch('builtins.input')    
     def test_even_noteven(self, mock_input):
         mock_input.return_value=7
         with self.assertRaises(CustomError):
             even(9)
             
-    @mock.patch('builtins.input')    
+    @patch('builtins.input')    
     def test_even_toosmall(self, mock_input):
         mock_input.return_value=2
         with self.assertRaises(CustomError):
@@ -30,16 +30,16 @@ class gbtest(unittest.TestCase):
         er=eratosthenes(8)
         self.assertEqual(er, [2, 3, 5, 7])
         
-    @mock.patch('Goldbachs.eratosthenes')
+    @patch('Goldbachs.eratosthenes')
     def test_oddprimes(self, mock_eratosthenes):
         mock_eratosthenes.return_value=[2, 3, 5, 7]
         op=odd_primes(8)
         self.assertEqual(op, [3, 5, 7])
         
-    @mock.patch('os.system')
-    @mock.patch('builtins.input')  
-    @mock.patch('Goldbachs.even')    
-    @mock.patch('Goldbachs.odd_primes')
+    @patch('os.system')
+    @patch('builtins.input')  
+    @patch('Goldbachs.even')    
+    @patch('Goldbachs.odd_primes')
     def test_goldbach(self, mock_oddprimes , mock_even, mock_input, os):
         mock_input.return_value=8
         mock_even.return_value=True, 8
--- HW8/02_end_test.py
+++ HW8/02_end_test.py
@@ -1,10 +1,11 @@
 import unittest
 from unittest import mock
-from Goldbachs import even, eratosthenes, goldbach, odd_primes, CustomError
+from Goldbachs import goldbach
 
+@mock.patch('os.system')
 @mock.patch('builtins.input')  
 class gbtest(unittest.TestCase):
-    def test_goldbach(self, mock_input):
+    def test_goldbach(self, mock_input, os):
         mock_input.return_value=8
         gb=goldbach()
         self.assertEqual(gb, (3,5))
