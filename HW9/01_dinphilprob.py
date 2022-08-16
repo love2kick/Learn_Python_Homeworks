b36ec7c4b8a15175f0d2fcdcb05778dc75845961
MOSCOW\Daniil_Bogdanov <daniil_bogdanov@epam.com>
2022-08-16 13:17:16 +0400

    Dining philosophers

---
+++ HW9/01_dinphilprob.py
@@ -0,0 +1,46 @@
+import threading
+import time
+from threading import Lock
+
+class DiningPhilosophers(threading.Thread):
+    hungry=True
+    def __init__(self, phil, lfork, rfork):
+        threading.Thread.__init__(self)
+        #forks = threads that will be locked, philosopher = main point
+        self.phil=phil
+        self.lfork=lfork
+        self.rfork=rfork
+    
+    def run(self):
+        while self.hungry:
+            lfork, rfork = self.lfork, self.rfork
+            time.sleep(2)
+            print(f"Philosopher {self.phil} is now hungry and wants to eat!")
+            while self.hungry:
+                lfork.acquire()
+                print(f"Philosopher {self.phil} takes fork {self.phil} on his left.")
+                if rfork.acquire(False):
+                    lfork.release()
+                    print(f"Philosopher {self.phil} puts down fork {self.phil} on his left.")
+                    break
+                else:
+                    print(f"Philosopher {self.phil} takes fork {self.phil+1} on his right.")
+                    print(f"Philosopher {self.phil} starts eating.")
+                    time.sleep(1)
+                    print(f"Philosopher {self.phil} is full.")
+                    lfork.release()
+                    rfork.release()
+                    self.hungry=False
+            else:
+                return
+def dine():
+    forks=[threading.Semaphore() for n in range(5)]
+    philosophers=[DiningPhilosophers(i, forks[i%5], forks[(i+1)%5]) for i in range(5)]
+    DiningPhilosophers.hungry=True
+    for d in philosophers:
+        d.start()
+    time.sleep(10)
+    print("Dinner is finished.")
+        
+if __name__=="__main__":
+    dine()
---
+++ HW9/exercises.py
@@ -0,0 +1,28 @@
+from asyncio import as_completed
+import time
+import threading
+import concurrent.futures
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f"Sleeping {seconds} second(s)...")
+    time.sleep(seconds)
+    return f"Done sleeping...{seconds}"
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs=[5,4,3,2,1]
+    results=executor.map(do_something, secs)
+    for result in results:
+        print
+#threads=[]
+#for _ in range(10):
+#    t=threading.Thread(target=do_something, args=[1.5])
+#    t.start()
+#    threads.append(t)
+#    
+#for thread in threads:
+#    thread.join()
+
+finish= time.perf_counter()
+print(f'Finished in {round(finish-start,2)} second(s)')
